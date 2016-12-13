481097d9027551bc75778da4f4744743ffbd9580
Jah <kennedye2@vcu.edu>
2016-12-13 11:54:57 -0500

    ykya project from here on it. finished video 11 and added delete and check methods on front and backend

--- server/publish.jsx
+++ server/publish.jsx
@@ -1,1 +1,8 @@
-Resolutions = new Mongo.Collection("resolutions");  
+Resolutions = new Mongo.Collection("resolutions");
+
+Meteor.publish("allResolutions", function(){
+  return Resolutions.find();
+  //return Resolutions.find({complete:false});
+  //return Resolutions.find({complete:true});
+  //^Publish the data you want
+});
