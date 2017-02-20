26218511626411fb0d16e0e0f4f8c1b70f41cd32
Jah <kennedye2@vcu.edu>
2017-02-19 23:22:12 -0500

    autoSuggest component implemented smoothly in both sections

--- client/Illustrators/CategoryFormI.jsx
+++ client/Illustrators/CategoryFormI.jsx
@@ -1,7 +1,33 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
+import AutoSuggest from '../autoComplete/AutoSuggest.jsx';
 
-export default class CategoryFormI extends Component {
+
+export default class CategoryFormI extends TrackerReact(React.Component) {
+  constructor(props) {
+    super(props);
+    this.state = {suggestionsOn: false, food: '', keyCount: 0};
+    //binds handleChange function to any event of the category input bar
+    this.handleChange = this.handleChange.bind(this);
+    //bings handleKeyPress event to any keyboard event when input is focused on
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  //handleKeyPress() and handleChange() are both for feeding the AutoSuggest component
+  //both channel their data through state.
+  handleKeyPress(e) {
+    if (e.keyCode == '40') {
+      var countIncrement = this.state.keyCount+1;
+      this.setState({keyCount: countIncrement});
+    }
+  }
+
+  handleChange(e){    //handles changes in the upper category form input
+    this.setState({suggestionsOn: true, keyCount: 0});
+    var input = e.target.value.trim();
+    !input ? this.setState({food: ''}):this.setState({food: input})
+  }
 
   selectCategory(event){
     event.preventDefault();
@@ -21,18 +47,29 @@ export default class CategoryFormI extends Component {
 
   render(){
     //these three variables are for feeding the AutoSuggest component in the return()
-    /*var autosuggestFood = this.state.food;
+    var autosuggestFood = this.state.food;
     var keyCount = this.state.keyCount;
-    var autosuggestActive = this.state.suggestionsOn;*/
+    var autosuggestActive = this.state.suggestionsOn;
     //////
     return (
             <form className="select-category"
               onSubmit={this.selectCategory.bind(this)}
               onBlur={this.selectCategory.bind(this)}>
-            <input type="text"
-              ref="currentCategory"
-              placeholder="Illustrator" />
-            //<AutoSuggest input={autosuggestFood} count={keyCount} active={autosuggestActive}/>
+
+              <label>
+
+                <input type="text"
+                  ref="currentCategory"
+                  placeholder="Illustrator"
+                  onChange={this.handleChange}
+                  onKeyDown={this.handleKeyPress}/>
+
+                <AutoSuggest input={autosuggestFood}
+                  count={keyCount}
+                  active={autosuggestActive}/>
+
+              </label>
+
             </form>
         )
     }
