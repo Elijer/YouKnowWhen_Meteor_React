1790f5346141f90264343f87743732c4b71ed8c9
Jah <kennedye2@vcu.edu>
2017-05-01 23:06:57 -0400

    highlighting of picSort toggle works

--- client/Muggles/SortingDashboard.jsx
+++ client/Muggles/SortingDashboard.jsx
@@ -2,23 +2,39 @@ import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
-export default class SortDash extends Component {
+export default class SortDash extends TrackerReact(React.Component) {
+
+  constructor(){
+    super();
+    this.state = {
+      picsFirst: false
+    }
+  }
 
   picsFirst(){
-      Session.set("picsFirst", true);
-      console.log(Session.get('picsFirst'));
+    this.setState({picsFirst: true});
   }
 
   picsLast(){
-    Session.set("picsFirst", false);
-    console.log(Session.get('picsFirst'));
+    this.setState({picsFirst: false});
   }
 
   render(){
+    var picButton;
+    var noPicButton;
+    var picsFirst = this.state.picsFirst;
+    if (picsFirst){
+      picButton = "selectedPicButton";
+      noPicButton = "unSelectedPicButton";
+    } else {
+      picButton = "unSelectedPicButton";
+      noPicButton = "selectedPicButton";
+    }
+
     return(
-      <div className="btn-group" role="group" aria-label="ButtonsYo">
-        <button type="button" className="btn btn-default" id = "picButton" onClick = {this.picsFirst}>Pics First</button>
-        <button type="button" className="btn btn-default" id = "picButton" onClick = {this.picsLast}>No Pics First</button>
+      <div className="btn-group" id = "btnPicSort" role="group" aria-label="ButtonsYo">
+        <button type="button" className="btn btn-lg" id = {picButton} onClick = {this.picsFirst.bind(this)}>Pics First</button>
+        <button type="button" className="btn btn-lg" id = {noPicButton} onClick = {this.picsLast.bind(this)}>No Pics First</button>
       </div>
     )
   }
